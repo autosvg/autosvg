7886837dec7cf5886db5da679a128eea5b34c05b
Thibaut Quentin <thibaut.quentin@laposte.net>
2016-02-02 18:31:53 +0100

    using getBoundClientRect for set the SVG's size

--- app/controller.js
+++ app/controller.js
@@ -8,10 +8,10 @@ function create_controller () {
     var canvas_container = document.getElementById("autoimg"); 
     var canvas = document.createElement("svg");
     var circle = document.createElement("circle");
-    canvas.setAttribute("width", canvas_container.getAttribute("width"));
-    canvas.setAttribute("height", canvas_container.getAttribute("height"));
-    circle.setAttribute("cx", canvas.getAttribute("width"));
-    circle.setAttribute("cy", canvas.getAttribute("height"));
+    canvas.setAttribute("width", canvas_container.getBoundingClientRect().width);
+    canvas.setAttribute("height", canvas_container.getBoundingClientRect().height);
+    circle.setAttribute("cx", 10);
+    circle.setAttribute("cy", 10);
     circle.setAttribute("r", 10);
     canvas.appendChild(circle);
     canvas_container.appendChild(canvas);
