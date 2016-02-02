1aa18532c9a428746740609ea5d83715ae628e12
Thibaut Quentin <thibaut.quentin@laposte.net>
2016-02-02 19:36:28 +0100

    Corrected create_controller function

--- app/controller.js
+++ app/controller.js
@@ -3,15 +3,16 @@
 module.exports = create_controller;
 
 function create_controller () {
+  //SVG namespace
+  var namespace = "http://www.w3.org/2000/svg";
   var bGeneration = document.getElementById("bGeneration");
   bGeneration.addEventListener("click", function () {
     var canvas_container = document.getElementById("autoimg"); 
-    var canvas = document.createElement("svg");
-    var circle = document.createElement("circle");
-    canvas.setAttribute("width", canvas_container.getBoundingClientRect().width);
-    canvas.setAttribute("height", canvas_container.getBoundingClientRect().height);
-    circle.setAttribute("cx", 10);
-    circle.setAttribute("cy", 10);
+    var canvas = document.createElementNS(namespace, "svg");
+    var circle = document.createElementNS(namespace, "circle");
+    /* The three followings lines defines a circle of center 0,0 and radius 10*/
+    circle.setAttribute("cx", 0);
+    circle.setAttribute("cy", 0);
     circle.setAttribute("r", 10);
     canvas.appendChild(circle);
     canvas_container.appendChild(canvas);
