e51cb58dceda4c4f6a202bc72b4dee67f59a298e
Yacine Hmito <yacine.hmito@gmail.com>
2016-02-04 23:13:16 +0100

    Refined start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-if [ -z "$BROWSER" ]; then
-  echo "You should set the BROWSER environment variable"
-else
-  (sleep 1 && $BROWSER http://localhost:3333/ &> /dev/null) & brunch watch --server
+if [[ -z "$BROWSER" ]]; then
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Set the BROWSER environment variable to use a specific browser"
+    BROWSER=open
+  elif command -v xdg-open 2>/dev/null; then
+    echo "Set the BROWSER environment variable to use a specific browser"
+    BROWSER=xdg-open
+  else
+    echo "You must set the BROWSER environment variable to the appropriate command"
+    return 1
+  fi
 fi
+
+(sleep 1 && $BROWSER http://localhost:3333/ &> /dev/null) & brunch watch --server
