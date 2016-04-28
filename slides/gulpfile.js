6a23bfeb7ddc0f50e438132a621048f2187ffd52
Yacine Hmito <yacine.hmito@gmail.com>
2016-04-28 22:33:04 +0200

    Onstage plugin

--- slides/gulpfile.js
+++ slides/gulpfile.js
@@ -3,9 +3,12 @@ var pkg = require('./package.json'),
   autoprefixer = require('gulp-autoprefixer'),
   browserify = require('browserify'),
   buffer = require('vinyl-buffer'),
+  chmod = require('gulp-chmod'),
+  closureCompiler = require('google-closure-compiler').gulp(),
   connect = require('gulp-connect'),
   csso = require('gulp-csso'),
   del = require('del'),
+  exec = require('gulp-exec'),
   ghpages = require('gh-pages'),
   gulp = require('gulp'),
   gutil = require('gulp-util'),
@@ -13,12 +16,32 @@ var pkg = require('./package.json'),
   path = require('path'),
   plumber = require('gulp-plumber'), // plumber prevents pipe breaking caused by errors thrown by plugins
   rename = require('gulp-rename'),
+  replace = require('gulp-replace'),
   source = require('vinyl-source-stream'),
   stylus = require('gulp-stylus'),
   through = require('through'),
+  tidy = require('tidy-html5').tidy_html5,
   uglify = require('gulp-uglify'),
-  chmod = require('gulp-chmod'),
-  exec = require('gulp-exec'),
+  closureCompilerOpts = {
+    compilation_level: 'SIMPLE', // ADVANCED breaks bespoke-fullscreen
+    warning_level: 'QUIET',
+    language_in: 'ES5_STRICT',
+    language_out: 'ES5'
+  },
+  tidyOpts = {
+    'anchor-as-name': 'false',
+    'coerce-endtags': 'false',
+    'drop-empty-elements': 'false',
+    'fix-uri': 'false',
+    'indent': 'false',
+    'literal-attributes': 'true',
+    'newline': 'LF',
+    'preserve-entities': 'true',
+    'quiet': 'true',
+    'show-warnings': 'false',
+    'tidy-mark': 'false',
+    'wrap': '0'
+  },
   isDist = process.argv.indexOf('deploy') >= 0;
 
 gulp.task('js', ['clean:js'], function() {
@@ -28,20 +51,43 @@ gulp.task('js', ['clean:js'], function() {
     .on('error', function(e) { if (isDist) { throw e; } else { gutil.log(e.stack); this.emit('end'); } })
     .pipe(source('src/scripts/main.js'))
     .pipe(buffer())
-    .pipe(isDist ? uglify() : through())
+    // hack to make bullets array available on deck object
+    .pipe(replace('activateBullet(0, 0);', 'deck.bullets = bullets; activateBullet(0, 0);'))
+    .pipe(isDist ? closureCompiler(closureCompilerOpts) : uglify())
     .pipe(rename('build.js'))
-    .pipe(gulp.dest('public/build'))
+    .pipe(gulp.dest('dist/build'))
     .pipe(connect.reload());
 });
 
-gulp.task('html', ['clean:html'], function() {
+gulp.task('jade-html', ['clean:jade-html'], function() {
+  return gulp.src('src/index.jade')
+    .pipe(isDist ? through() : plumber())
+    .pipe(jade({ pretty: '  ' }))
+    .pipe(rename('index-jade.html'))
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('asciidoc-html', ['clean:asciidoc-html'], function() {
   return gulp.src('src/index.adoc')
     .pipe(isDist ? through() : plumber())
+    // NOTE using stdin here would cause loss of context
     .pipe(exec('asciidoctor-bespoke -o - src/index.adoc', { pipeStdout: true }))
     .pipe(exec.reporter({ stdout: false }))
+    .pipe(through(function(file) {
+      var html = tidy(file.contents.toString(), tidyOpts) // NOTE based on tidy 4.9.26
+        // strip extra newlines inside <pre> tags (fixed in tidy 5.1.2)
+        .replace(new RegExp('<pre([^>]*)>\\n([\\s\\S]*?)\\n</pre>', 'g'), '<pre$1>$2</pre>\n')
+        // strip extra newline after <script> start tag for empty and single-line content
+        .replace(new RegExp('>\\n(?:(.+)\\n)?</script>', 'g'), '>$1</script>')
+        // add newline before <script> tags
+        .replace(new RegExp('><script([^>]*)>', 'g'), '>\n<script$1>');
+      file.contents = new Buffer(html);
+      this.push(file);
+    }))
     .pipe(rename('index.html'))
     .pipe(chmod(644))
-    .pipe(gulp.dest('public'))
+    .pipe(gulp.dest('dist'))
     .pipe(connect.reload());
 });
 
@@ -52,62 +98,56 @@ gulp.task('css', ['clean:css'], function() {
     .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
     .pipe(isDist ? csso() : through())
     .pipe(rename('build.css'))
-    .pipe(gulp.dest('public/build'))
-    .pipe(connect.reload());
-});
-
-gulp.task('fonts', ['clean:fonts'], function() {
-  return gulp.src('src/fonts/*')
-    .pipe(gulp.dest('public/fonts'))
+    .pipe(gulp.dest('dist/build'))
     .pipe(connect.reload());
 });
 
 gulp.task('images', ['clean:images'], function() {
   return gulp.src('src/images/**/*')
-    .pipe(gulp.dest('public/images'))
+    .pipe(gulp.dest('dist/images'))
     .pipe(connect.reload());
 });
 
 gulp.task('clean', function() {
-  return del('public');
+  return del('dist');
 });
 
-gulp.task('clean:html', function() {
-  return del('public/index.html');
+gulp.task('clean:jade-html', function() {
+  return del('dist/index-jade.html');
+});
+
+gulp.task('clean:asciidoc-html', function() {
+  return del('dist/index.html');
 });
 
 gulp.task('clean:js', function() {
-  return del('public/build/build.js');
+  return del('dist/build/build.js');
 });
 
 gulp.task('clean:css', function() {
-  return del('public/build/build.css');
-});
-
-gulp.task('clean:fonts', function() {
-  return del('public/fonts');
+  return del('dist/build/build.css');
 });
 
 gulp.task('clean:images', function() {
-  return del('public/images');
+  return del('dist/images');
 });
 
 gulp.task('connect', ['build'], function() {
-  connect.server({ root: 'public', port: 8000, livereload: true });
+  connect.server({ root: 'dist', port: 8000, livereload: true });
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/**/*.adoc', ['html']);
+  gulp.watch('src/**/*.adoc', ['asciidoc-html']);
+  gulp.watch('src/**/*.jade', ['jade-html']);
   gulp.watch('src/scripts/**/*.js', ['js']);
   gulp.watch('src/styles/**/*.styl', ['css']);
   gulp.watch('src/images/**/*', ['images']);
-  gulp.watch('src/fonts/*', ['fonts']);
 });
 
 gulp.task('deploy', ['clean', 'build'], function(done) {
-  ghpages.publish(path.join(__dirname, 'public'), { logger: gutil.log }, done);
+  ghpages.publish(path.join(__dirname, 'dist'), { logger: gutil.log }, done);
 });
 
-gulp.task('build', ['js', 'html', 'css', 'fonts', 'images']);
+gulp.task('build', ['js', 'asciidoc-html', 'jade-html', 'css', 'images']);
 gulp.task('serve', ['connect', 'watch']);
 gulp.task('default', ['build']);
--- slides/src/scripts/main.js
+++ slides/src/scripts/main.js
@@ -1,22 +1,50 @@
-var isWebKit = 'webkitAppearance' in document.documentElement.style,
-  // zoom-based scaling causes font sizes and line heights to be calculated differently
-  // on the other hand, zoom-based scaling correctly anti-aliases fonts during transforms (no need for layer creation hack)
-  scaleMethod = isWebKit ? 'zoom' : 'transform',
-  bespoke = require('bespoke'),
+var bespoke = require('bespoke'),
   bullets = require('bespoke-bullets'),
   classes = require('bespoke-classes'),
+  onstage = require('bespoke-onstage');
+  //cursor = require('bespoke-cursor'),
+  //forms = require('bespoke-forms'),
   fullscreen = require('bespoke-fullscreen'),
   hash = require('bespoke-hash'),
   nav = require('bespoke-nav'),
   overview = require('bespoke-overview'),
-  scale = require('bespoke-scale');
+  scale = require('bespoke-scale'),
+  title = require('bespoke-title');
 
-bespoke.from({ parent: 'article.deck', slides: 'section' }, [
+// NOTE address small gap above slide in Chrome/WebKit by scaling from top of viewport; only works in landscape
+//if ('webkitAppearance' in document.documentElement.style) {
+//  var style = document.createElement('style');
+//  style.textContent = '.bespoke-scale-parent{transform-origin:50% 0%}\n.bespoke-slide{top:0;margin-top:0}';
+//  document.head.appendChild(style); 
+//}
+
+(window.bespoke = bespoke).deck = bespoke.from('.deck', [
   classes(),
   nav(),
   fullscreen(),
-  (scaleMethod ? scale(scaleMethod) : function(deck) {}),
-  overview({ columns: 4 }),
-  bullets('.build, .build-items > *:not(.build-items)'),
+  onstage(),
+  scale('transform'), // NOTE zoom-based scaling produces slightly different results than scale transform
+  // overview({ margin: 300, autostart: true, title: true, numbers: true }),
+  bullets('.build,.build-items>*:not(.build-items)'),
+  function(deck) { // NOTE only works with hack to bespoke-bullets to expose bullets array
+    deck.on('deactivate', function(e) {
+      if (e.preview || !e.slide.classList.contains('bespoke-active') || deck.parent.classList.contains('bespoke-overview')) return;
+      var bullets = deck.bullets[e.index] || [];
+      if (bullets.length > 0) {
+        bullets.forEach(function(bullet) {
+          ['bespoke-bullet',
+           'bespoke-bullet-active',
+           'bespoke-bullet-inactive',
+           'bespoke-bullet-current'].forEach(function(cl) { bullet.classList.remove(cl); });
+        });
+        bullets.length = 0;
+      }
+    });
+  },
+  title(),
+  // enable forms() if you have form elements in your slides
+  //forms(),
+  // enable cursor() to automatically hide the cursor when presenting
+  //cursor(),
   hash()
 ]);
